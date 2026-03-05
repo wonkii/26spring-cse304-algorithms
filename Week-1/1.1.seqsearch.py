bf1112df7778760ad56a325983b9c48735fc0adc
wonkii <wkddnjswns0368@khu.ac.kr>
2026-03-05 20:24:26 +0900

    .

--- Week-1/1.1.seqsearch.py
+++ Week-1/1.1.seqsearch.py
@@ -2,6 +2,12 @@ from typing import List
 
 def seqsearch(n: int, S: List[int], x: int) -> int: 
     location = 0
+    
+    for i in S:
+        if i == x:
+            return location
+        location = location + 1
 
+    location = -1
     # Complete the code here
     return location
---
+++ Week-1/LeetCode.py
@@ -0,0 +1,9 @@
+def maxProfit(self, prices):
+    List = []
+    for i in prices:
+        
+
+
+
+# 각 요소를 다음 부터 끝까지 빼고 가장 큰 값을 List에 기록
+        
