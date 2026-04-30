c04aee8bcf16efc1523eac5daad84c54d50d448a
wonkii <wkddnjswns0368@khu.ac.kr>
2026-04-30 15:05:08 +0900

    .

--- Week-7/4.1.prim.py
+++ Week-7/4.1.prim.py
@@ -5,11 +5,24 @@ def prim(n: int, W: List[List[float]]) -> List[Tuple[int, int, float]]:
     nearest = [-1] * (n + 1)
     distance = [-1] * (n + 1)
     INF = float("inf")
+
     for i in range(2, n + 1):
         nearest[i] = 1
         distance[i] = W[1][i]
+
     for _ in range(n - 1):
         min = INF
         # Complete the code here
+        for i in range(2, n + 1):
+            if(0 <= distance[i] < min):
+                min = distance[i]
+                vnear = i
+        F.append((vnear, nearest[vnear], min))
+        distance[vnear] = -1
+
+        for i in range(2, n + 1):
+            if(W[i][vnear] < distance[i]):
+                distance[i] = W[i][vnear]
+                nearest[i] = vnear
 
     return F
