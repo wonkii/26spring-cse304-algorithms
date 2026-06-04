e9f61811bca09096163f0bc8541a874dc1c387a5
wonkii <wkddnjswns0368@khu.ac.kr>
2026-06-04 15:39:10 +0900

    .

--- Week-12/7.1.insertion.sort.py
+++ Week-12/7.1.insertion.sort.py
@@ -1,4 +1,11 @@
 from typing import List
 
 def insertionsort(n: int, S: List[int]) -> None:
-    # Complete the code here
+    for i in range(1, n):
+        x = S[i]
+        j = i - 1
+
+        while(j >= 0 and S[j] > x):
+            S[j+1] = S[j]
+            j = j -1
+        S[j+1] = x
--- Week-12/7.2.selection.sort.py
+++ Week-12/7.2.selection.sort.py
@@ -1,4 +1,11 @@
 from typing import List
 
 def selectionsort(n: int, S: List[int]) -> None:
-    # Complete the code here
+    for i in range(0, n):
+        smallest = i
+        for j in range(i+1, n):
+            if S[j] < S[smallest]:
+                smallest = j
+        x = S[i]
+        S[i] = S[smallest]
+        S[smallest] = x
